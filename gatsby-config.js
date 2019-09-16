9240bad082314cd5a071123ce8f19570e37e46b2
sagarnasit <sagarnasit@gmail.com>
2019-09-16 19:36:26 +0530

    Remove duplicate plugin config for manifest setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,18 +17,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    {
       resolve: "gatsby-source-graphql",
       options: {
         // This type will contain remote schema Query type
