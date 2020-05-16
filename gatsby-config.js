271239f980a93b1e09b067d1e64fab626f95c221
Sagar Nasit <sagarnasit@gmail.com>
2020-05-16 23:16:17 +0530

    Update package for gatsby theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,44 +1,48 @@
-module.exports = {
-  siteMetadata: {
-    title: `Gatsby Woocommerce`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-  },
-  plugins: [
-    `gatsby-plugin-emotion`,
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+module.exports = options => {
+
+  return {
+    siteMetadata: {
+      title: `Gatsby Woocommerce`,
+      description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+      author: `@gatsbyjs`,
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: "gatsby-source-graphql",
-      options: {
-        // This type will contain remote schema Query type
-        typeName: "WPGraphQL",
-        // This is field under which it's accessible
-        fieldName: "wpgraphql",
-        // Url to query from
-        url: "http://woo.gkdevil.in/graphql",
+    plugins: [
+      `gatsby-plugin-emotion`,
+      `gatsby-plugin-react-helmet`,
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          name: `images`,
+          path: `${__dirname}/src/images`,
+        },
       },
-    },
-    `gatsby-plugin-offline`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Woocommerce Store`,
-        short_name: `Woocommerce Store`,
-        start_url: `/`,
-        background_color: `#f7f0eb`,
-        theme_color: `#a2466c`,
-        display: `standalone`,
-        icon: `src/images/gatsby-icon.png`,
+      `gatsby-transformer-sharp`,
+      `gatsby-plugin-sharp`,
+      {
+        resolve: "gatsby-source-graphql",
+        options: {
+          // This type will contain remote schema Query type
+          typeName: "WPGraphQL",
+          // This is field under which it's accessible
+          fieldName: "wpgraphql",
+          // Url to query from
+          url: options.storeUrl || `"http://woocommerce.com/graphql"`,
+        },
       },
-    },
-  ],
+      `gatsby-plugin-offline`,
+      {
+        resolve: `gatsby-plugin-manifest`,
+        options: {
+          name: `Woocommerce Store`,
+          short_name: `Woocommerce Store`,
+          start_url: `/`,
+          background_color: `#f7f0eb`,
+          theme_color: `#a2466c`,
+          display: `standalone`,
+          icon: `${__dirname}/src/images/gatsby-icon.png`,
+        },
+      },
+    ],
+  }
+
 }
